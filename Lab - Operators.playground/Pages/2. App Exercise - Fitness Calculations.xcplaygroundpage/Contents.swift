8b2395b1a3da8751d25b5ed47c4e0db33eeee2f8
Anthony Pisciotta <apisciotta1@live.maryville.edu>
2018-09-08 14:05:37 -0500

    lab 1.3

--- Lab - Operators.playground/Pages/2. App Exercise - Fitness Calculations.xcplaygroundpage/Contents.swift
+++ Lab - Operators.playground/Pages/2. App Exercise - Fitness Calculations.xcplaygroundpage/Contents.swift
@@ -5,8 +5,12 @@
  
  Your fitness tracker keeps track of users' heart rate, but you might also want to display their average heart rate over the last hour. Create three constants, `heartRate1`, `heartRate2`, and `heartRate3`. Give each constant a different value between 60 and 100. Create a constant `addedHR` equal to the sum of all three heart rates. Now create a constant called `averageHR` that equals `addedHR` divided by 3 to get the average. Print the result.
  */
-
-
+let heartRate1 = 65
+let heartRate2 = 76
+let heartRate3 = 79
+let addedHR = heartRate1 + heartRate2 + heartRate3
+let averageHR = addedHR / 3
+print(averageHR)
 /*:
  Now create three more constants, `heartRate1D`, `heartRate2D`, and `heartRate3D`, equal to the same values as `heartRate1`, `heartRate2`, and `heartRate3`. These new constants should be of type `Double`. Create a constant `addedHRD` equal to the sum of all three heart rates. Create a constant called `averageHRD` that equals the `addedHRD` divided by 3 to get the average of your new heart rate constants. Print the result. Does this differ from your previous average? Why or why not?
  */
