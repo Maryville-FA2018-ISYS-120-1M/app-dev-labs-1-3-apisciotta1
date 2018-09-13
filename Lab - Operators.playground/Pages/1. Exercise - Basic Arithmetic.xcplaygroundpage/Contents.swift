a1ec172d1617c17c0448fb5f435fcf9ba77a0eda
Shanae Ullman <shanae.ullman@gmail.com>
2018-09-13 14:39:09 -0500

    SU edits

--- Lab - Operators.playground/Pages/1. Exercise - Basic Arithmetic.xcplaygroundpage/Contents.swift
+++ Lab - Operators.playground/Pages/1. Exercise - Basic Arithmetic.xcplaygroundpage/Contents.swift
@@ -3,6 +3,7 @@
  
  You decide to build a shed and want to know beforehand the area of your yard that it will take up. Create two constants, `width` and `height`, with values of 10 and 20, respectively. Create an `area` constant that is the result of multiplying the two previous constants together, and print out the result.
  */
+// SU edits
 let width = 10
 let height = 20
 let area = height*width
@@ -21,13 +22,14 @@ print(perimeter)
 /*:
  Print what you would expect the result of integer division of 10 divided by 3 to be. Create a constant, `integerDivisionResult` that is the result of 10 divided by 3, and print the value.
  */
+// print(3)
 let integerDivisionResult = 10/3
 print(integerDivisionResult)
 /*:
  Now create two constants, `double10` and `double3`, set to 10 and 3, and declare their types as `Double` values. Declare a final constant `divisionResult` equal to the result of `double10` divided by `double3`. Print the value of `divisionResult`. How does this differ from the value when using integer division?
  */
-let double10 = 10.0
-let double3 = 3.0
+let double10 = 10.0 // let double10: Double = 10
+let double3 = 3.0 // let double3: Double = 3
 let divisionResult = double10/double3
 print(divisionResult)
 /*:
@@ -43,4 +45,5 @@ let diameter = 2 * radius
 let circumfrence = 2 * pi * radius
 print(diameter)
 print(circumfrence)
+//good job
 //: page 1 of 8  |  [Next: App Exercise - Fitness Calculations](@next)
